9a1318288e01b38ed507bb778be4a75d65a87df8
lakshika <lakshikamedhavini@gmail.com>
2022-11-17 16:03:04 +0530

    test

--- SSD_Backend-master/Tests/message.test.js
+++ SSD_Backend-master/Tests/message.test.js
@@ -16,14 +16,14 @@ describe("Add new message", () => {
   });
 });
 
-// describe("POST /messages/messages", () => {
-//   it("should create a msg", async () => {
-//     const res = await request(app).post("/messages/messages").send({
-//       message: "hi",
-//       sender: "5f38c4f9eb0ca06da4102b58de6a924e",
-//       encryptedMsg: "a533b2d31573847331ba2af55f7fe9c0",
-//     });
-//     expect(res.statusCode).toBe(201);
-//     expect(res.body.price).toBe(104);
-//   });
-// });
+describe("POST /messages/messages", () => {
+  it("should create a msg", async () => {
+    const res = await request(app).post("/messages/messages").send({
+      message: "hi",
+      sender: "5f38c4f9eb0ca06da4102b58de6a924e",
+      encryptedMsg: "a533b2d31573847331ba2af55f7fe9c0",
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.body.price).toBe(104);
+  });
+});
--- SSD_Backend-master/Tests/user.test.js
+++ SSD_Backend-master/Tests/user.test.js
@@ -3,7 +3,7 @@ const app = require("../server");
 const { HttpStatusCode } = require("axios");
 
 describe("Add new user", () => {
-  it("Check authenitcation of add new user end point", async () => {
+  it(" authenitcation - add new user", async () => {
     const res = await request(app)
       .post("/users/create")
       .set({ token: "In valid token", Accept: "application/json" })
@@ -16,20 +16,20 @@ describe("Add new user", () => {
   });
 });
 
-// describe("POST /users/create", () => {
-//   it("should create a user", async () => {
-//     const res = await request(app)
-//       .post("/users/create")
-//       .set({
-//         token:
-//           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9uIjpbIjAyIiwiMDMiXSwicm9sZSI6Ik1hbmFnZXIiLCJpZCI6IjYzNzViZjIyZmM0MDNkN2NjNDdkOTQxYyIsImlhdCI6MTY2ODY3MzM3MiwiZXhwIjoxNjcxMjY1MzcyfQ.Vo8Iq38xT1QrsUb0eq2vIjVm_emqHj5IEmJR8P2Xq8c",
-//         Accept: "application/json",
-//       })
-//       .send({
-//         username: "abc",
-//         password: "abc",
-//         role: "Admin",
-//       });
-//     expect(res.statusCode).toBe(201);
-//   });
-// });
+describe("POST /users/create", () => {
+  it("should create a user", async () => {
+    const res = await request(app)
+      .post("/users/create")
+      .set({
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9uIjpbIjAyIiwiMDMiXSwicm9sZSI6Ik1hbmFnZXIiLCJpZCI6IjYzNzViZjIyZmM0MDNkN2NjNDdkOTQxYyIsImlhdCI6MTY2ODY3MzM3MiwiZXhwIjoxNjcxMjY1MzcyfQ.Vo8Iq38xT1QrsUb0eq2vIjVm_emqHj5IEmJR8P2Xq8c",
+        Accept: "application/json",
+      })
+      .send({
+        username: "abc",
+        password: "abc",
+        role: "Admin",
+      });
+    expect(res.statusCode).toBe(201);
+  });
+});
